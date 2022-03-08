c55aff9f0edb0733676fb377533fe40606751983
dingbo <dingbo8128@163.com>
2022-03-08 15:22:04 +0800

    remove unused component

--- src/theme/TOC/SearchBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './styles.module.css'; // Using a custom className
-import clsx from 'clsx';
-
-export default function SearchBar(props) {
-    return (<div className={styles.searchBarWrapper}>
-
-    </div>);
-}
